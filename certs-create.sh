8bf157737654474e29370a1b791794aadd1d9263
Vincent de Saboulin <vincent.desaboulin@gmail.com>
2021-03-31 22:56:31 +0200

    test http connector mTLS #757

--- certs-create.sh
+++ certs-create.sh
@@ -13,7 +13,7 @@ TAG=6.1.0
 # Generate CA key
 docker run -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${TAG} openssl req -new -x509 -keyout /tmp/snakeoil-ca-1.key -out /tmp/snakeoil-ca-1.crt -days 365 -subj '/CN=ca1.test.confluent.io/OU=TEST/O=CONFLUENT/L=PaloAlto/ST=Ca/C=US' -passin pass:confluent -passout pass:confluent
 
-for i in http-service-mtls-auth
+for i in http-service-mtls-auth http-service-ssl-basic-auth
 do
     echo "------------------------------- $i -------------------------------"
 
@@ -22,14 +22,14 @@ do
                  -alias $i \
                  -dname "CN=$i,OU=TEST,O=CONFLUENT,L=PaloAlto,S=Ca,C=US" \
                                  -ext "SAN=dns:$i,dns:localhost" \
-                 -keystore /tmp/keystore.jks \
+                 -keystore /tmp/keystore.$i.jks \
                  -keyalg RSA \
                  -storepass confluent \
                  -keypass confluent \
                  -storetype pkcs12
 
     # Create the certificate signing request (CSR)
-    docker run -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${TAG} keytool -keystore /tmp/keystore.jks -alias $i -certreq -file /tmp/$i.csr -storepass confluent -keypass confluent -ext "SAN=dns:$i,dns:localhost"
+    docker run -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${TAG} keytool -keystore /tmp/keystore.$i.jks -alias $i -certreq -file /tmp/$i.csr -storepass confluent -keypass confluent -ext "SAN=dns:$i,dns:localhost"
         #openssl req -in $i.csr -text -noout
 
 cat << EOF > extfile
@@ -51,15 +51,15 @@ EOF
         #openssl x509 -noout -text -in $i-ca1-signed.crt
 
         # Sign and import the CA cert into the keystore
-    docker run -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${TAG} keytool -noprompt -keystore /tmp/keystore.jks -alias CARoot -import -file /tmp/snakeoil-ca-1.crt -storepass confluent -keypass confluent
+    docker run -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${TAG} keytool -noprompt -keystore /tmp/keystore.$i.jks -alias CARoot -import -file /tmp/snakeoil-ca-1.crt -storepass confluent -keypass confluent
         #keytool -list -v -keystore kafka.$i.keystore.jks -storepass confluent
 
         # Sign and import the host certificate into the keystore
-    docker run -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${TAG}  keytool -noprompt -keystore /tmp/keystore.jks -alias $i -import -file /tmp/$i-ca1-signed.crt -storepass confluent -keypass confluent -ext "SAN=dns:$i,dns:localhost"
+    docker run -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${TAG}  keytool -noprompt -keystore /tmp/keystore.$i.jks -alias $i -import -file /tmp/$i-ca1-signed.crt -storepass confluent -keypass confluent -ext "SAN=dns:$i,dns:localhost"
         #keytool -list -v -keystore kafka.$i.keystore.jks -storepass confluent
 
     # Create truststore and import the CA cert
-    docker run -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${TAG}  keytool -noprompt -keystore /tmp/truststore.jks -alias CARoot -import -file /tmp/snakeoil-ca-1.crt -storepass confluent -keypass confluent
+    docker run -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${TAG}  keytool -noprompt -keystore /tmp/truststore.$i.jks -alias CARoot -import -file /tmp/snakeoil-ca-1.crt -storepass confluent -keypass confluent
 
     # Save creds
       echo  "confluent" > ${i}_sslkey_creds
@@ -70,11 +70,11 @@ EOF
     #   openssl x509 -noout -modulus -in client.certificate.pem | openssl md5
     #   openssl rsa -noout -modulus -in client.key | openssl md5
     #   log "GET /" | openssl s_client -connect localhost:8085/subjects -cert client.certificate.pem -key client.key -tls1
-    docker run -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${TAG} keytool -export -alias $i -file /tmp/$i.der -keystore /tmp/keystore.jks -storepass confluent
+    docker run -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${TAG} keytool -export -alias $i -file /tmp/$i.der -keystore /tmp/keystore.$i.jks -storepass confluent
     docker run -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${TAG} openssl x509 -inform der -in /tmp/$i.der -out /tmp/$i.certificate.pem
-    docker run -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${TAG} keytool -importkeystore -srckeystore /tmp/keystore.jks -destkeystore /tmp/$i.keystore.p12 -deststoretype PKCS12 -deststorepass confluent -srcstorepass confluent -noprompt
+    docker run -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${TAG} keytool -importkeystore -srckeystore /tmp/keystore.$i.jks -destkeystore /tmp/$i.keystore.p12 -deststoretype PKCS12 -deststorepass confluent -srcstorepass confluent -noprompt
     docker run -v $PWD:/tmp vdesabou/kafka-docker-playground-connect:${TAG} openssl pkcs12 -in /tmp/$i.keystore.p12 -nodes -nocerts -out /tmp/$i.key -passin pass:confluent
-done
 
-cp keystore.jks src/main/resources/keystore.jks
-cp truststore.jks src/main/resources/truststore.jks
+    cp keystore.$i.jks src/main/resources/keystore.$i.jks
+    cp truststore.$i.jks src/main/resources/truststore.$i.jks
+done
